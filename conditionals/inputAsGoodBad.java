6fd67ede88b6cdc1f3253f2c6646b8b6ec7cbad3
sajda <sajda.kbir@gmail.com>
2022-12-30 22:57:23 +0530

    input as good or bad

---
+++ conditionals/inputAsGoodBad.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public static String trueOrFalse(boolean teaching) {
+        if(teaching)return "Good";
+        return "Bad";
+    }
+
+    public static void main(String[] args) {
+        boolean teaching =true;
+        assert(trueOrFalse(teaching).equals("Good")) : "Expect \"Good\" for teaching = true";
+        System.out.println("All test cases in main function passed");
+    }
+}
