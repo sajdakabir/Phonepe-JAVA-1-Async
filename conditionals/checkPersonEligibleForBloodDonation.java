d00d059bc7b971a688325e6a40082533d6f5e395
sajda <sajda.kbir@gmail.com>
2022-12-31 09:47:23 +0530

    eligible for blood

--- conditionals/checkPersonEligibleForBloodDonation.java
+++ conditionals/checkPersonEligibleForBloodDonation.java
@@ -14,3 +14,29 @@ public class Solution {
 
     }
 }
+
+// another way
+
+ import java.util.*;
+
+public class Solution {
+
+  public static String checkBloodDonationEligiblity(int age, int weight) {
+    if(age >= 18) {
+      if (weight > 50) {
+        return "Eligible";
+      } else {
+        return "NotEligible";
+      }
+    } else {
+      return "UnderAge";
+    }
+  }
+
+  public static void main(String args[]) {
+        assert (checkBloodDonationEligiblity(25, 55) == "Eligible") : "Expect Eligible for a = 25, b = 55";
+        System.out.println("All test cases in main function passed");
+
+    }
+}
+ 
